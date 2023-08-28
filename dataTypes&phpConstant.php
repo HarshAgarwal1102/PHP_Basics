a0dfae58bd2e969be6aec57a1a5958d4e30c77ea
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-28 18:25:25 +0530

    Create dataTypes&phpConstant.php

---
+++ dataTypes&phpConstant.php
@@ -0,0 +1,13 @@
+/*
+              Data types 
+Data type is the classification of data into a category.
+  
+Alphanumeric characters are classified as STRINGS
+Whole numbers are classified as INTEGERS
+Numbers with decimal points are classified as FLOAT
+True or false values are classified as BOOLEAN
+A collection of similar types of elements are called ARRAY
+An object is a specific instance of a class that serves as a templates for OBJECTS
+
+As told earlier, PHP determines the data types by analyzing the values of the data supplied.
+*/

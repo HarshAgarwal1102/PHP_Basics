2662348c458928dad78165438f7d12c8ca647347
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-28 18:27:44 +0530

    Update dataTypes&phpConstant.php

--- dataTypes&phpConstant.php
+++ dataTypes&phpConstant.php
@@ -11,3 +11,22 @@ An object is a specific instance of a class that serves as a templates for OBJEC
 
 As told earlier, PHP determines the data types by analyzing the values of the data supplied.
 */
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class="container">
+    <?php
+        define('PI', 3.14);    // defining a PHP constant
+        $radius = 15;
+        $area = PI * $radius * $radius;
+        echo $area;
+    ?>
+    </div>
+</body>
+</html>

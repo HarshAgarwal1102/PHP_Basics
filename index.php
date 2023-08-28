6f61df16ba0a5f757d3147e7ad72ab64a4003acf
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-28 17:49:25 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Tutorial</title>
+</head>
+<body>
+    <div class="container">
+    This is my first PHP website
+      <?php
+        echo "Hello world and this is printed using PHP";
+      ?>
+    </div>
+</body>
+</html>

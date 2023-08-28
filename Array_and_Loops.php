458dd9da2a17586e49ea2ad37824251b0c1a9a71
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-28 18:31:48 +0530

    Create Array_and_Loops.php

---
+++ Array_and_Loops.php
@@ -0,0 +1,43 @@
+<?echo
+  // Arrays in php
+    $languages = array("Python", "C++", "php", "NodeJs"); 
+    // echo count($languages);   
+    // echo $languages[0];   
+
+
+  // Loops in PHP
+    $a = 0;
+    while ($a <= 10) {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    }
+
+    // Iterating arrays in PHP using while loop
+    $a = 0;
+    while ($a < count($languages)) {
+        echo "<br>The value of language is: ";
+        echo $languages[$a];
+        $a++;
+    }
+
+    // Do while loop
+    $a = 200;
+    do {
+        echo "<br>The value of a is: ";
+        echo $a;
+        $a++;
+    } while ($a < 10);
+
+    // For loop
+    for ($a=60; $a < 10; $a++) { 
+        echo "<br>The value of a from the for loop is: ";
+        echo $a;
+    }
+
+    foreach ($languages as $value) {
+        echo "<br>The value from foreach loop is ";
+        echo $value;
+    }
+
+?>

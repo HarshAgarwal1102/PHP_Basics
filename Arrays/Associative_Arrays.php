32f005de474a965623b297863297b676489e14fc
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-30 00:01:14 +0530

    Create Associative_Arrays.php

---
+++ Arrays/Associative_Arrays.php
@@ -0,0 +1,18 @@
+<?php
+
+echo "Associative arrays in php ";
+ 
+// Associative arrays
+$favCol = array('shubham' => 'red','rohan'=> 'green',
+                    'harry'=> 'brown', 8=>'this' );
+
+                    // echo $favCol['harry'];
+                    // echo "<br>";
+                    // echo $favCol['rohan'];
+                    // echo "<br>";
+                    // echo $favCol[8]; 
+foreach ($favCol as $key => $value) {
+    echo "<br>Favorite color of $key is $value";
+}
+
+?>

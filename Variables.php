55124f4ac6ebfdb92b8f1579c70778b78437da44
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-29 23:40:06 +0530

    Create Variables.php

---
+++ Variables.php
@@ -0,0 +1,17 @@
+<?php
+
+// Keywords aren't case sensitive.
+echo "About Variables..<br>";
+eCho "Nice to see you here..<br>";
+
+// Variables are containers for storing information
+// Starts with a ($) symbol.
+$name = "Rohan";
+// Variables are case sensitive
+// $namE = "Shubham";
+$income = 20000;
+echo "This guy is $name and his income is Rs. $income<br>";
+echo "$name is a good boy<br>";
+echo "$name is the real person<br>";
+
+?>

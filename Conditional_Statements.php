585d3c91183918d35782b57b5d4be115d30c6351
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-29 23:50:45 +0530

    Update Conditional_Statements.php

--- Conditional_Statements.php
+++ Conditional_Statements.php
@@ -24,3 +24,30 @@ if-else-if
         echo "You can not go to the party";
     }
 ?>
+
+
+// Switch Statements.
+
+<?php
+
+$age = 56;
+
+switch($age){
+    case 12:
+        echo "You are 12 years old <br>";
+        break;
+
+    case 45:
+        echo "You are 45 years old <br>";
+        break;
+
+    case 56:
+        echo "You are 56 years old boy <br>";
+        break;
+    
+    default:
+        echo "Your age is weird <br>";
+        break;
+
+    }
+?>

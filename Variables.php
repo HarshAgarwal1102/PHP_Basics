5665dffe1b9e950f8a56d87801e2b7bb45c9ec2b
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-29 23:47:15 +0530

    Update Variables.php

--- Variables.php
+++ Variables.php
@@ -1,5 +1,14 @@
 <?php
 
+echo "Rules for creating variables in php <br>";
+echo "1. Starts with a $ sign <br>";
+echo "2. Cannot start with a number <br>";
+echo "3. Must start with a letter or an underscore character <br>";
+echo "4. Can only contain alphanumeric characters and underscores <br>";
+echo "5. Variables in php are Case sensitive. ($harry, $hArry and $Harry are different) <br>";
+echo "<br>";
+
+
 // Keywords aren't case sensitive.
 echo "About Variables..<br>";
 eCho "Nice to see you here..<br>";

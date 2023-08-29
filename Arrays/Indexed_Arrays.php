bb09d1e3aadf879323654f02170b2fb5ca2fb68b
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-30 00:00:01 +0530

    Create Indexed_Arrays.php

---
+++ Arrays/Indexed_Arrays.php
@@ -0,0 +1,13 @@
+<?php
+
+echo "Arrays in php ";
+// These are called indexed arrays:
+$arr = array('this', 'that', 'what');
+echo $arr[0];
+echo "<br>";
+echo $arr[1];
+echo "<br>";
+echo $arr[2]; 
+echo "<br>";
+
+?>

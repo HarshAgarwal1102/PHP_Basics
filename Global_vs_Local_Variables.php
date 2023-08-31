3c1eecedda4881c8cee3cb1d05aff836f58301dd
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-31 20:44:05 +0530

    Update Global_vs_Local_Variables.php

--- Global_vs_Local_Variables.php
+++ Global_vs_Local_Variables.php
@@ -1,10 +1,13 @@
 <?php
 echo "Welcome to scope and local/global vars in php<br>";
-$a = 98; // Global Variable
+$a = 98; // Global Variable  // Global variables are declared outside the functions and are available to direct access globally.
 $b = 9;  // Global Variable
 
 function printValue(){
+    // local variables cannot be accessed outside the function.
     // $a = 97; // Local Variable
+
+    // PHP provides a function “global” to access the global variables inside the function.
     global $a, $b; // Give me the access to this global variable
     $a = 100;
     $b = 1000;

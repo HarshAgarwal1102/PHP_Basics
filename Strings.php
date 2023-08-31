01e6d09f80a3b7282e0bd5efafe8eef852a26a13
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-31 21:46:38 +0530

    Update Strings.php

--- Strings.php
+++ Strings.php
@@ -7,6 +7,15 @@
     echo "The reversed string is ". strrev($str) . ". Thank you <br>";  // inbuilt function to reverse the string
     echo "The search for is in this string is ". strpos($str, "is") . ". Thank you <br>";  // inbuilt function to find the starting index of the provided letters in the string
     echo "The replaced string is ". str_replace("is", "at", $str) . ". Thank you <br>";  // inbuilt function to replace the present word with the provided in the string
+    
+    $name = "Rohan is a good boy";
+    echo str_repeat($name, 4);
+    echo "<br>";
+    echo "<pre>";
+    echo rtrim("    this is a good boy     ");
+    echo "<br>";
+    echo ltrim("    this is a good boy     ");
+    echo "</pre>";
 ?>
 
 
@@ -18,7 +27,7 @@
 
 3)strrev(): This function returns the reverse of a string, taking string as the parameter.
 
-4)strops(): The strpos() function finds the position of the first occurrence of a string inside another string. 
+4)strpos(): The strpos() function finds the position of the first occurrence of a string inside another string. 
 
 One important part to keep in mind is, The PHP concatenation operator (.) is used to combine two string values to create one string.
 

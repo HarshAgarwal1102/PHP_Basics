3cf0cc68b85e1541d3e17011120d94555b901f66
Harsh Agarwal <harshagarwal.4404@gmail.com>
2023-08-31 20:35:04 +0530

    Update Date_Function.php

--- Date_Function.php
+++ Date_Function.php
@@ -1,5 +1,11 @@
 <?php
 
+// d	Day of the month, 2 digits with leading zeros	01 to 31
+// j	Day of the month without leading zeros	1 to 31
+// S	English ordinal suffix for the day of the month, 2 characters	st, nd, rd or th. Works well with j
+// F	A full textual representation of a month, such as January or March	January through December
+// Y	A full numeric representation of a year, at least 4 digits, with - for years BCE.	Examples: -0055, 0787, 1999, 2003, 10191
+
 echo "Welcome to the world of dates<br>";
 // $d = date("dS F Y");
 $d = date("dS F Y, g:i A");
